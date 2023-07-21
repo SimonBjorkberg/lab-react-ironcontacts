649aa6c59108eb21e8c50737658864ae694af954
Simon Björkberg <127056123+SimonBjorkberg@users.noreply.github.com>
2023-07-21 03:43:26 +0200

    DONE

--- src/App.js
+++ src/App.js
@@ -18,34 +18,69 @@ function App() {
       setContacts(newArray)
     }
   }
+  
+  function sortByName() {
+    const sortedArray = [...contacts].sort((a, b) => {
+      if(a.name < b.name) {
+        return -1;
+      } else if (a.name > b.name) {
+        return 1;
+      }
+      return 0;
+    })
+    setContacts(sortedArray)
+  }
+
+  function sortByPopularity() {
+    const sortedArray = [...contacts].sort((a, b) => {
+      if (a.popularity > b.popularity) {
+        return -1;
+      }
+      else if (a.popularity < b.popularity) {
+        return 1;
+      }
+      return 0;
+     })
+    setContacts(sortedArray)
+  }
+  function deleteButton(contactId) {
+    const newArray = [...contacts].filter(contact => {
+      return contact.id !== contactId
+    })
+    setContacts(newArray)
+  }
 
   return (
     <div className="App">
-    <h1 className="text-2xl">Ironhack Contacts</h1>
-    <button onClick={() => addContact()} className="bg-blue-300">Add new random contact</button>
-      <table className="mx-auto">
+    <h1 className="text-4xl my-5">Ironhack Contacts</h1>
+    <button onClick={() => addContact()} className="bg-blue-800 text-white py-2 px-5 mx-5 rounded-md hover:bg-blue-600">Add new random contact</button>
+    <button className="bg-blue-800 text-white py-2 px-5 mx-5 rounded-md hover:bg-blue-600" onClick={() => sortByPopularity()}>Sort by popularity</button>
+    <button className="bg-blue-800 text-white py-2 px-5 mx-5 rounded-md hover:bg-blue-600" onClick={() => sortByName()}>Sort by name</button>
+      <table className="mx-auto border-2 border-black mt-5">
         <tbody>
-        <tr>
+        <tr className="border-2 border-black">
         <td className="w-32">Picture</td>
         <td className="w-32">Name</td>
         <td className="w-32">Popularity</td>
-        <td className="w-32">Won <br /> Oscar</td>
-        <td className="w-32">Won <br /> Emmy</td>
+        <td className="w-32">Won Oscar</td>
+        <td className="w-32">Won Emmy</td>
+        <td className="w-32">Actions</td>
         </tr>
           {contacts.map((contact) => {
             return (
               <tr key={contact.id}>
                 <td>
-                  <img className="w-32" src={contact.pictureUrl} alt="profile pic" />{" "}
+                  <img className="w-32 mt-2" src={contact.pictureUrl} alt="profile pic" />{" "}
                 </td>
                 <td>
-                <h2>{contact.name}</h2>{" "}
+                <h2 className="font-bold ml-5">{contact.name}</h2>{" "}
                 </td>
                 <td>
                 {Math.round(contact.popularity)}
                 </td>
-                <td>{contact.wonOscar && 'Won'}</td>
-                <td>{contact.wonEmmy && 'Won'}</td>
+                <td>{contact.wonOscar && 'Yes'}</td>
+                <td>{contact.wonEmmy && 'Yes'}</td>
+                <td><button className="py-2 px-5 bg-red-700 rounded-md hover:bg-red-900 text-white" onClick={() => deleteButton(contact.id)}>Delete</button></td>
               </tr>
             );
           })}
