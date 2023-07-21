da8029f51d26201d4ab1563695684c86be23d405
Simon Björkberg <127056123+SimonBjorkberg@users.noreply.github.com>
2023-07-21 02:15:28 +0200

    temp solution

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import allContacts from "./contacts.json";
+import "./App.css";
+import { useState } from "react";
+
+const firstFiveContacts = allContacts.slice(0, 5);
 
 function App() {
+  const [ contacts, setContacts ] = useState(firstFiveContacts)
+
+  function addContact() {
+    const randomNumber = Math.floor(Math.random() * allContacts.length)
+    const randomContact = allContacts[randomNumber]
+    if (contacts.some((contact) => contact.id === randomContact.id)) {
+      console.log('Contact exists')
+    }
+    else {
+      const newArray = [...contacts, randomContact]
+      setContacts(newArray)
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1 className="text-2xl">Ironhack Contacts</h1>
+    <button onClick={() => addContact()} className="bg-blue-300">Add new random contact</button>
+      <table className="mx-auto">
+        <tbody>
+        <tr>
+        <td className="w-32">Picture</td>
+        <td className="w-32">Name</td>
+        <td className="w-32">Popularity</td>
+        <td className="w-32">Won <br /> Oscar</td>
+        <td className="w-32">Won <br /> Emmy</td>
+        </tr>
+          {contacts.map((contact) => {
+            return (
+              <tr key={contact.id}>
+                <td>
+                  <img className="w-32" src={contact.pictureUrl} alt="profile pic" />{" "}
+                </td>
+                <td>
+                <h2>{contact.name}</h2>{" "}
+                </td>
+                <td>
+                {Math.round(contact.popularity)}
+                </td>
+                <td>{contact.wonOscar && 'Won'}</td>
+                <td>{contact.wonEmmy && 'Won'}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
